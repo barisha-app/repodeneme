3032d805ee6443aaf2d454dcf9388400e85b9835
Barisha <appbarisha@gmail.com>
2026-03-14 02:49:03 +0300

    Update BenimPlugin.kt

--- BenimPlugin/BenimPlugin.kt
+++ BenimPlugin/BenimPlugin.kt
@@ -1,1 +1,12 @@
 
+package com.barisha
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class BenimPlugin : Plugin() {
+    override fun load() {
+        registerMainAPI(BenimProvider())
+    }
+}
