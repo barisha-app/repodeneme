10c2e10481bd8bad55e7dc12e1a27904b182eca3
Barisha <appbarisha@gmail.com>
2026-03-14 02:54:40 +0300

    Create build.gradle.kts

---
+++ BenimPlugin/build.gradle.kts
@@ -0,0 +1,9 @@
+version = 1
+
+cloudstream {
+    language = "tr"
+    authors = listOf("barisha-app")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    description = "Benim ilk Cloudstream eklentim"
+}
