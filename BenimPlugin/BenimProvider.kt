49e30895983594fc16cf1456d3c007a7ea039a6c
Barisha <appbarisha@gmail.com>
2026-03-14 03:49:17 +0300

    Update mainUrl to example.com

--- BenimPlugin/BenimProvider.kt
+++ BenimPlugin/BenimProvider.kt
@@ -1,11 +1,8 @@
-package com.barisha
-
 import com.lagradost.cloudstream3.MainAPI
 import com.lagradost.cloudstream3.TvType
 
 class BenimProvider : MainAPI() {
     override var name = "BenimPlugin"
-    override var mainUrl = "https://senin-site-adresin.com"
+    override var mainUrl = "https://example.com"
     override val supportedTypes = setOf(TvType.Movie, TvType.TvSeries)
-    override var lang = "tr"
 }
